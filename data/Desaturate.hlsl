cccec6c77a2a76794c2dc804cbf7e9a502a6f20c
Michael Geary <mg@mg.to>
2016-05-07 17:47:32 -0700

    Scatter plot experiment

--- data/Desaturate.hlsl
+++ data/Desaturate.hlsl
@@ -22,13 +22,33 @@ void writeToPixel( int x, int y, float3 colour ) {
 	int ired = (int)( clamp( colour.r, 0, 1 ) * 255 );
 	int igreen = (int)( clamp( colour.g, 0, 1 ) * 255 ) << 8;
 	int iblue = (int)( clamp( colour.b, 0, 1 ) * 255 ) << 16;
-	
-	BufferOut[ index ].colour = ired + igreen + iblue;
+
+	BufferOut[index].colour = ired + igreen + iblue;
+}
+
+int clampRGB( float value ) {
+	return (int)( clamp( value, 0, 1 ) * 255 );
 }
 
 [numthreads( 32, 16, 1 )]
 void CSMain( uint3 dispatchThreadID : SV_DispatchThreadID ) {
 	float3 pixel = readPixel( dispatchThreadID.x, dispatchThreadID.y );
+#if 0
 	pixel.rgb = pixel.r * 0.3 + pixel.g * 0.59 + pixel.b * 0.11;
-	writeToPixel( dispatchThreadID.x, dispatchThreadID.y, pixel );
+	writeToPixel( dispatchThreadID.x, dispatchThreadID.y, pixel.rgb );
+#else
+	int r = clampRGB( pixel.r );
+	int g = clampRGB( pixel.g );
+	int b = clampRGB( pixel.b );
+	int color = r | ( g << 8 ) | ( b << 16 );
+	int x = g;
+	int y = 256 - b;
+	int index = x + y * 1024;
+	float pixOld;
+	InterlockedXor(
+		BufferOut[index].colour,
+		color,
+		pixOld
+	);
+#endif
 }
